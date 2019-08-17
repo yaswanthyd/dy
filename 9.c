e6e5accaee40dd6f6f7cdda7812c3ccab832b6cc
yaswanthyd <48052584+yaswanthyd@users.noreply.github.com>
2019-08-17 10:30:15 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	     int n,k,sum=0;
+	     printf("enter tha value");
+	     scanf("%d%d",&n,&k);
+	     int a[n];
+	     for (int i=0;i<n;i++);
+	     {
+	     scanf("%d",&a[n]);
+	     }
+	     for(int i=0;i<k;i++);
+	    {
+	        sum+=a[n];
+	     }
+	     printf("%d",sum);
+	}
+
