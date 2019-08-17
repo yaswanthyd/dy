bff2f2991f9435da1b61aaf0ba3fe38b929c2a6e
yaswanthyd <48052584+yaswanthyd@users.noreply.github.com>
2019-08-17 10:54:17 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+	main()
+	{
+	int n1, n2, i, flag;
+	printf("Enter two positive integers: ");
+	scanf("%d %d", &n1, &n2);
+	printf("Prime numbers between %d and %d are: ", n1, n2);
+	for(i=n1+1; i<n2; ++i)
+	{
+	if(flag == 1)
+	printf("%d ",i);
+	}
+	return 0;
+	}
+	int j, flag = 1;
+	for(j=2; j <= n/2; ++j)
+	{
+	if (n%j == 0)
+	{
+	flag =0;
+	break;
+	}
+	}
+	return flag;
+	}
