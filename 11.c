5b9d0be6656c6898865d89737bfde11f9a13d0ee
yaswanthyd <48052584+yaswanthyd@users.noreply.github.com>
2019-08-17 10:35:28 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 main()
 {
-    int base, exponent;
-
-    long long result = 1;
-
+    int base,exponent;
+    long long result=1;
     printf("Enter a base number: ");
-    scanf("%d", &base);
-
+    scanf("%d",&base);
     printf("Enter an exponent: ");
-    scanf("%d", &exponent);
-
-    while (exponent != 0)
+    scanf("%d",&exponent);
+    while(exponent!=0)
     {
-        result *= base;
+        result*=base;
         --exponent;
     }
-
-    printf("Answer = %lld", result);
-
+    printf("Answer=%lld",result);
     return 0;
 }
