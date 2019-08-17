b408545a7e4d7cf21fb1df89af4eca9b74b89398
yaswanthyd <48052584+yaswanthyd@users.noreply.github.com>
2019-08-17 10:58:34 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+	main()
+	{
+	int i,n,fact=1;
+	printf("Enter a number to calculate its factorial\n");
+	scanf("%d",&n);
+	for(i= 1;i<=n;i++)
+	fact=fact*i;
+	printf("Factorial of %d = %d\n",n,fact);
+	}
